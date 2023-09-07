24b07467175b24332af9e01e79c599ff026f0caf
Kristófer Helgi Sigurðsson <kristoferhelgi@protonmail.com>
2023-09-07 15:22:40 +0000

    Working file of Project 3 with notes breaking up the assignment

---
+++ Project_3,_Find_Integers.py
@@ -0,0 +1,14 @@
+#Write input 1: 1 integer, 20 <= stop_range <= 100
+#Write input 2: 1 integer, 1 <= num_divisors <= 12
+
+#Step 1
+#Use range(10, stop_range) to cycle through numbers
+#Find the sum of the numbers integers
+    #Look at Assignment 3.5
+#See if that sum squared is an integer
+#Write a list of the numbers under stop_range which meet these criteria and if they do print them.
+
+#Step 2
+#Use range(10, stop_range) to cycle through numbers
+#If the number mod(num_divisors) == 0 then that number works
+#Print out all numbers which work
